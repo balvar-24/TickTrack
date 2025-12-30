60b4211f18839d8a0ea4ff1e19f8b2a2c088c834
Benjamin Alvarez Arndt <250857916+balvar-24@users.noreply.github.com>
2025-12-30 16:29:00 +0100

    First Changes to the landing site

--- src/App.jsx
+++ src/App.jsx
@@ -8,28 +8,28 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>TickTrack</h1>
+      <p>Manage your tickets more efficiently.</p>
+      <div className="login-box">
+        <h2>Login</h2>
+        <div className="input-box">
+          <span className="icon">
+            <ion-icon name="person-circle-outline"></ion-icon>
+          </span>
+          <input type="text"/>
+          <label>Username</label>
+        </div>
+        <div className="input-box">
+          <span className="icon">
+            <ion-icon name="lock-closed-outline"></ion-icon>
+          </span>
+          <input type="text"/>
+          <label>Password</label>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
+
 
 export default App
