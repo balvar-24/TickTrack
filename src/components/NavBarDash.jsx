9ae1c684676c5d3f6194b99e19a829ca9dab316b
Benjamin Alvarez Arndt <250857916+balvar-24@users.noreply.github.com>
2026-01-03 22:54:42 +0100

    Added 'components' folder to structurize the project / Added seperate NavBar files for login and dashboard with shared NavBar.ss

---
+++ src/components/NavBarDash.jsx
@@ -0,0 +1,17 @@
+import TickTrackSiteLogo from '/TickTrackSiteLogo.png'
+import './NavBar.css'
+
+function NavBarDash() {
+  return (
+    <>
+      <div className="NavigationBar">
+        <div className="logo">
+          <img src={TickTrackSiteLogo} alt="TickTrack Logo" width="40" height="40"/>
+        </div>
+        <label style={{ color: 'white', fontWeight: 'bold', fontSize: '1.2rem' }}>TickTrack</label>
+      </div>
+    </>
+  )
+}
+
+export default NavBarDash
