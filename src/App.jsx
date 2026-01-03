7cc1959ad33e846a6f12a72baf902f6504315f6e
Benjamin Alvarez Arndt <250857916+balvar-24@users.noreply.github.com>
2026-01-03 23:04:32 +0100

    Imported routing/navigation functions - Added routes - Moved code to 'Login.jsx'

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,15 @@
-import { useState } from 'react'
-import TickTrackSiteLogo from '/TickTrackSiteLogo.png'
-import './App.css'
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
 
 function App() {
   return (
-    <>
-      <div className="NavigationBar">
-        <div className="logo">
-          <img src={TickTrackSiteLogo} alt="TickTrack Logo" width="40" height="40"/>
-        </div>
-        <label style={{ color: 'white', fontWeight: 'bold', fontSize: '1.2rem' }}>TickTrack</label>
-      </div>
-
-      <h1>TickTrack</h1>
-      <p>Manage your tickets more efficiently.</p>
-      <div className="login-box">
-        <h2>Login</h2>
-        <div className="input-box">
-          <span className="icon">
-            <ion-icon name="person-circle-outline">
-            </ion-icon>
-          </span>
-          <input type="username" required/>
-          <label>Username</label>
-        </div>
-        <div className="input-box">
-          <span className="icon">
-            <ion-icon name="lock-closed-outline">
-            </ion-icon>
-          </span>
-          <input type="password" required/>
-          <label>Password</label>
-        </div>
-        <div className="remember-forgot">
-          <label><input type="checkbox"/>Remember me</label>
-          <a href="#">Forgot Password?</a>
-        </div>
-        <button type="submit" className="">Login</button>
-        <div className="login-register">
-          <p>Don't have an account? <a href="#">Register</a></p>
-        </div>
-      </div>
-    </>
-  )
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="*" element={<Navigate to="/login" />} />
+    </Routes>
+  );
 }
 
-
-export default App
+export default App;
