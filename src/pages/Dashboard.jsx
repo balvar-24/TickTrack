46e275cebd6e61d99e88055cb8be61cf0d2eca71
Benjamin Alvarez Arndt <250857916+balvar-24@users.noreply.github.com>
2026-01-04 23:55:53 +0100

    Integrated the shortcut bar into the dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,14 @@
 import './Dashboard.css';
 import '../App.css';
 import NavBarDash from '../components/NavBarDash.jsx';
+import ShortcutBar from '../components/ShortcutBar.jsx';
 
 function Dashboard() {
     return (
+        
         <div className="dashboard-container">
             <NavBarDash />
+            <ShortcutBar />
             <div className="dashboard-content">
                 <h1>Welcome to the Dashboard</h1>
             </div>
