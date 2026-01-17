894075d72e2fc8a550a15a64dbbb13390c755e98
Benjamin Alvarez Arndt <250857916+balvar-24@users.noreply.github.com>
2026-01-17 19:23:02 +0100

    Added navigation handling logic for buttons

--- src/components/ShortcutBar.jsx
+++ src/components/ShortcutBar.jsx
@@ -1,20 +1,32 @@
-import { useState } from 'react';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import './ShortcutBar.css'
 
-function ShortcutBar() {
+export default function ShortcutBar() {
+    const navigate = useNavigate();
+
+    const handleChanges = () => {
+    navigate("/changes");
+    }
+    const handleIncidents = () => {
+    navigate("/incidents");
+    }
+    const handleProblems = () => {
+    navigate("/problems");
+    }
 
     return (
         <div className="shortcut-bar">
-            <button type="button">
+            <button type="button" onClick={handleChanges}>
                 Changes
             </button>
-            <button type="button">
+            <button type="button" onClick={handleIncidents}>
                 Incidents
             </button>
-            <button type="button">
+            <button type="button" onClick={handleProblems}>
                 Problems
             </button>
         </div>
+
     )
 }
-export default ShortcutBar;
